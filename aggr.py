6a80f25466fcb039f2a48950fe9b98c45c32c64b
Stefan Schramm <mail@stefanschramm.net>
2010-11-11 19:07:12 +0100

    license stuff + readme

--- aggr.py
+++ aggr.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
 
+# aggr.py - aggr - aggregate CSV-data and print on the standard output
+#
+# Copyright (C) 2010, Stefan Schramm <mail@stefanschramm.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import sys
 
 def parse_cmdline_arguments(args):
